f328ea9009420dec8973f5e6f4825824cfadc8e0
JEONGMUN LEE <74344761+leejeongmunn@users.noreply.github.com>
2022-12-30 15:35:27 +0900

    <Fix> url 변경
    url 수정

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("registration/", include("dj_rest_auth.registration.urls")),
-    path("", include("dj_rest_auth.urls")),
+    path("auth/", include("dj_rest_auth.urls")),
 ]
